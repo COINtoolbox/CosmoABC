90cc73a6e4c22e23fef9e0e33a75bf89190a0fd3
drArli <arlindo.trindade@gmail.com>
2014-08-28 20:19:30 +0100

    Clean Version

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,7 @@
-from math import *
-from gi.repository import GObject
-import matplotlib.pyplot as plt
-from gi.repository import NumCosmo as Nc
-from gi.repository import NumCosmoMath as Ncm
+import numpy as np
 
-from NCountSimul_true_mass_nw_v4 import *
+
+from NCount_Simul import *
 
 
 def generate_data(name):
@@ -14,13 +11,13 @@ def generate_data(name):
 
    zmin = 0.3              #minimum redshift
    zmax = 1.32             #maximum redshift
-   H0 = 71.15               #Hubble parameter
+   H0 = 70.0             #Hubble parameter
    Omegam = 0.262           #Dark matter density
    Omegab = 0.0439          #Baryon density
    Tgamma0 = 2.725          #Radiation temperature today
    ns = 0.97                #spectral index 
    sigma8 = 0.807           #sigma8
-   w = -1.01                #Dark energy equation of state     
+   w = -1.0                #Dark energy equation of state     
 
 #mass bin
 #dm = [5*10**13, 10**14, 10**14.25, 10**14.5, 10**14.75,  10**15, 10**15.25,  10**15.5, 10**15.75 ]
@@ -33,36 +30,28 @@ def generate_data(name):
    
    seed = 100 
    
-   CosmoParams=ChooseParamsInput()
 
-   CosmoParams.params={"H0":H0,"Ob":Omegab,"Om":Omegam,"OL":1.-Omegam-Omegab,"Tgamma":Tgamma0,"ns":ns,"sigma8":sigma8,"w":w}
+   CP={"H0":H0,"Ob":Omegab,"Om":Omegam,"OL":1.-Omegam-Omegab,"Tgamma":Tgamma0,"ns":ns,"sigma8":sigma8,"w":w}
 
-   ncount = NCountSimul (zmin, zmax, log ( dm[0] ), log ( 10**16 ), area )
+   ncount = NCountSimul (zmin, zmax, np.log ( dm[0] ), np.log ( 10**16 ), area )
    
-   data_fid = numpy.array( ncount.simulation( zmax, seed, CosmoParams.params)[1] )
+   data_fid = np.array( ncount.simulation( zmax, seed, CP )[1] )
    
    f=open(name+".params","w")
    
    f.write("Parameters used\n")
    f.write("zmin=%.3f\n" % zmin)
-   f.write("zmax=%.3f\n" % zmax)
-   f.write("H0=%.2f\n" % H0)
-   f.write("Omega_baryons=%.3f\n" % Omegab)
-   f.write("Omega_matter=%.3f\n" % Omegam)
-   f.write("Tgamma0=%.3f\n" % Tgamma0)
-   f.write("zmin=%.3f\n" % ns)
-   f.write("sigma8=%.3f\n" % sigma8)
-   f.write("w=%.3f\n" % w)
-   f.write("\n")
-   f.write("Mass bins\n")
-   for i in dm:
-     
-      f.write("%.4e\t" % i)
-
-   f.close()
+   f.write("zmax=%.3f" % zmax)
+   f.write("H0=%.2f" % H0)
+   f.write("Omega_baryons=%.3f" % Omegab)
+   f.write("Omega_matter=%.3f" % Omegam)
+   f.write("Tgamma0=%.3f" % Tgamma0)
+   f.write("zmin=%.3f" % ns)
+   f.write("sigma8=%.3f" % sigma8)
+   f.write("w=%.3f" % w)
    
    
-   numpy.savetxt(name+".dat",data_fid,fmt="%.4f\t %.4f")
+   np.savetxt(name+".dat",data_fid,fmt="%.4f\t %.4f")
    
    return data_fid
    
