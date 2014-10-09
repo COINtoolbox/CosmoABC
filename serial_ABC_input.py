e6b1f7b9de710ce1f5ed1eeb12664c81207ca2de
emilleishida <emilleishida@gmail.com>
2014-10-09 17:25:53 +0200

    change input file path

--- serial_ABC_input.py
+++ serial_ABC_input.py
@@ -43,7 +43,7 @@ area = 2500
 
 #######
 # path and name to data file
-mock_data = "/home/emille/Dropbox/WGC/ABC/the_return_of_abc/data/dataset_ABC_seed123.dat"
+mock_data = "dataset_ABC_seed123.dat"
 
 N=200      # particle sample size after the first iteration
 
