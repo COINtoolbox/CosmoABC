1bd92906e324a82e7ab6a9312116f5c393d2c0a2
emilleishida <emilleishida@gmail.com>
2015-04-20 23:29:26 +0200

    update doc for "extra" keyword in Parameters

--- CosmoABC/distances.py
+++ CosmoABC/distances.py
@@ -12,106 +12,119 @@ import numpy as np
 from scipy.interpolate import Rbf
 from scipy.stats.mstats import mquantiles
 
-def SumGRBF( dataset1, dataset2, Parameters):
+def SumGRBF(dataset1, dataset2, Parameters):
     """
-    Compute the sum of Gaussian Radial Basis Function (GRBF) for all points in data1 having data2 as a fixed reference basis.
-    Following equation 18 notation of Ishida et al., 2015: \mathcal{D} -> data1 and Y-> data2.  
+    Compute the sum of Gaussian Radial Basis Function (GRBF).  
 
-    input:	data1 -> first data matrix ( collumns -> features, lines -> objects )
-   		data2 -> second data matrix, fixed basis (collumns -> features, lines -> objects )
- 		s  (optional)-> smooth parameter (scalar)
-                f1 (optional) -> kernel function implemented in scipy.interpolate.Rbf rotine (string)
+    input: data1 -> first data matrix ( collumns -> features, lines -> objects )
+   	   data2 -> second data matrix, fixed basis (collumns -> features, 
+                                                     lines -> objects )
+ 	   Parameters -> dictionary of input parameters
 
-    output: scalar
+    output: distance -> scalar
     """
 
     #GRBF must be one in all points in the fixed basis sample
     one = [1 for j in range(len(dataset2))]
 
-    if len( dataset2[0] ) == 1:
+    if len(dataset2[0]) == 1:
 
-        rbf1 = Rbf( dataset2,  one, function=Parameters['kernel_func'], smooth=Parameters['s'] )
+        rbf1 = Rbf(dataset2,  one, function=Parameters['kernel_func'], 
+                   smooth=Parameters['s'])
     
-        #sum GRBF for all elements in data1 and centralize in the number of existing points in data2
-        sum1 = abs( np.nansum( [ np.log( rbf1( line[0] ) ) for line in dataset1 ]  ) - len( dataset2 ))
+        #sum GRBF for all elements in data1 and centralize in the number of 
+        #existing points in data2
+        sum1 = abs(np.nansum([np.log(rbf1(line[0])) for line in dataset1 ]) 
+                   - len( dataset2 ))
     
-    if len( dataset2[0] ) == 2:
+    if len(dataset2[0]) == 2:
 
-        rbf1 = Rbf( dataset2[:,0], dataset2[:,1], one, function=Parameters['kernel_func'], smooth=Parameters['s'] )
+        rbf1 = Rbf(dataset2[:,0], dataset2[:,1], one, 
+                   function=Parameters['kernel_func'], smooth=Parameters['s'])
     
-        #sum GRBF for all elements in data1 and centralize in the number of existing points in data2
-        sum1 = abs( np.nansum( [ np.log( rbf1( line[0], line[1] ) ) for line in dataset1 ]  ) - len( dataset2 ))
-            
-
+        #sum GRBF for all elements in data1 and centralize in the number of 
+        #existing points in data2
+        sum1 = abs(np.nansum([np.log(rbf1(line[0], line[1])) 
+                   for line in dataset1]) - len(dataset2))
+           
     return sum1 
         
 
 
-def distance_grbf( dataset2, Parameters ):
+def distance_grbf(dataset2, Parameters):
     """
-    Compute the distance between two samples.
-    Following equation 19 notation of Ishida et al., 2015: \mathcal{D} -> data1 and Y-> data2.  
-
-    input:	data1 -> first data matrix ( collumns -> features, lines -> objects )
-   		data2 -> second data matrix, fixed basis (collumns -> features, lines -> objects )
- 		s (optional) -> smooth parameter (scalar)
-                f (optional) -> kernel function implemented for GRBF rotine (string)
+    Compute the distance between two catalogues based on Gaussian Radial 
+    Basis function.
+    
+    input: dataset2 -> simulated catalogue
+           Parameters -> dictionary of input parameters
               
-    output: scalar
+    output: distance -> scalar
     """
 
-    if sum( dataset2[0] ) == 0 or len( dataset2 ) > 5*len(Parameters['dataset1']) or dataset2.shape[0] == 1:
+    if sum(dataset2[0]) == 0 or len(dataset2) > 5*len(Parameters['dataset1']) \ 
+                             or dataset2.shape[0] == 1:
         return 10**10
-     
-  
     else:
  
-        j1 = SumGRBF( Parameters['dataset1'], dataset2, Parameters)
+        j1 = SumGRBF(Parameters['dataset1'], dataset2, Parameters)
 
-        #distance based on the logarithm of GRBF and normalized by results from data1
+        #distance based on the logarithm of GRBF and normalized by 
+        #results from data1
         if j1 > 0 and str( j1 ) != 'nan' :
             d = abs( -2 * j1 + 2 * Parameters['extra'])
-            return [ d ]
+            return [d]
 
         else:
-            op1 = open( 'dataset2_error_source.dat', 'w' )
+            op1 = open('dataset2_error_source.dat', 'w')
             for line in dataset2:
                 for item in line:
-                    op1.write( str(item) + '    ' )
+                    op1.write(str(item) + '    ')
                 op1.write('\n')
             op1.close() 
                
-            raise ValueError('ERROR in function SumGRBF!!  Corresponding simulation is stored in file "dataset2_error.dat" ' )
+            raise ValueError('ERROR in function SumGRBF!!  Corresponding \
+                              simulation is stored in file \ 
+                              "dataset2_error.dat"')
 
 
 
 def summ_quantiles( dataset1, Parameters ):
+    """
+    Updates the dictionary of input parameters with the summary based on 
+    quantiles for the observed catalogue. 
+    ** This is an auxiliary function in order to optimize parallelization**
+
+    input: dataset1 -> observed catalogue
+   
+    output: Parameters -> updated dictionary of input parameters where the 
+                          keyword 'extra' correspond to the summary calculated 
+                          for the observed data set.
+    """
 
     
     Parameters['dist_dim'] = len(dataset1[0]) + 1
  
-    qlist = np.arange( 0.05, 1.0, 0.95/Parameters['quantile_nodes'] )
+    qlist = np.arange(0.05, 1.0, 0.95/Parameters['quantile_nodes'])
 
     Parameters['extra'] = []
-    for i1 in xrange( len( dataset1[0] ) ):
+    for i1 in xrange(len( dataset1[0])):
 
-        Parameters['extra'].append( np.array([ mquantiles( dataset1[:, i1], prob=item ) for item in qlist ]) )
-    
- 
+        Parameters['extra'].append(np.array([mquantiles(dataset1[:,i1], \ 
+                                   prob=item) for item in qlist]))
+   
     return Parameters
 
 
-def distance_quantiles( dataset2, Parameters ):
+def distance_quantiles(dataset2, Parameters):
     """
-    Compute the distance between two samples based on quantiles of the 2 first features.
-    Following equation 19 notation of Ishida et al., 2015: \mathcal{D} -> data1 and Y-> data2.  
+    Compute the distance between two samples based on quantiles of 
+    observed features.
 
-    input:	data1 -> first data matrix ( collumns -> features, lines -> objects )
-   		data2 -> second data matrix, fixed basis (collumns -> features, lines -> objects )
-                s -> extra parameter
-               
+    input: dataset2 -> simulated catalog
+           Parameters -> dictionary of input parameters	
               
-    output: scalar
+    output: distance -> scalar
     """
 
 
@@ -120,7 +133,8 @@ def distance_quantiles( dataset2, Parameters ):
 
     qd = []
     for j1 in xrange( len( dataset2[0] ) ):
-        qd.append( np.array([ mquantiles( dataset2[:, j1 ], prob=item ) for item in qlist ]) )
+        qd.append( np.array([ mquantiles( dataset2[:, j1 ], prob=item ) 
+                              for item in qlist ]) )
         
 
     l1 = len( Parameters['dataset1'] )
